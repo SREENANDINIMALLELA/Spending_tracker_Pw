bf5d1973624d5807eb1a2d14d373cfffead76f33
sreenandini <mallelasreenandini2@gmail.com>
2019-09-21 20:53:40 +0100

    added create merchant and category features

--- spending_tracker/transactions_controller.rb
+++ spending_tracker/transactions_controller.rb
@@ -1,6 +1,8 @@
 require( 'sinatra' )
 require( 'sinatra/contrib/all' )
 require_relative( 'models/transaction' )
+require_relative( 'models/merchant' )
+require_relative( 'models/category' )
 require_relative( 'dtos/transaction_dto' )
 require_relative( 'dtos/category_dto' )
 require_relative( 'dtos/merchant_dto' )
@@ -9,16 +11,33 @@ also_reload( '../models/*' )
 get '/spending_tracker' do
  @transactions = Transaction.all()
 
-erb(:"show_all")
+erb(:'transaction/show_all')
 end
 
 get '/spending_tracker/category' do
  @transactions = Transaction.find_transactions_by_category()
-erb(:"show_by_category")
+erb(:'category/show_by_category')
 end
 
 get '/spending_tracker/merchant' do
 @transactions = Transaction.find_transactions_by_merchant()
+erb(:"merchant/show_by_merchant")
+end
 
-erb(:"show_by_merchant")
+get'/spending_tracker/merchant/new'do
+erb(:"merchant/new")
+end
+post'/spending_tracker/merchant'do
+merchant = Merchant.new(params)
+merchant.save
+erb(:"merchant/create")
+end
+
+get'/spending_tracker/category/new'do
+erb(:"category/new")
+end
+post'/spending_tracker/category'do
+category = Category.new(params)
+category.save
+erb(:"category/create")
 end
