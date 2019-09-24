c4791f5ba68e4d0275fd100322fd7ff60dd8396b
sreenandini <mallelasreenandini2@gmail.com>
2019-09-24 22:10:49 +0100

    category dto updated and added to integer

--- spending_tracker/dtos/category_dto.rb
+++ spending_tracker/dtos/category_dto.rb
@@ -2,7 +2,7 @@ require_relative('../db/sql_runner')
 class CategoryDto
   attr_accessor :id ,:name
   def initialize(args)
-    @id = args['id']
+    @id = args['id'].to_i
     @name = args['name']
   end
 end
